15828e0f3bfda9d592dac9710bff239ab672974e
Addison Schuhardt <addison@schuhardt.net>
2021-08-07 21:40:08 -0700

    removing things i don't need
    Signed-off-by: Addison Schuhardt <addison@schuhardt.net>

--- main.c
+++ main.c
@@ -5,15 +5,12 @@
 // uncomment lines below to enable/disable features. See README.md for details
 #define CTEST_SEGFAULT
 //#define CTEST_NO_COLORS
-//#define CTEST_COLOR_OK
+#define CTEST_COLOR_OK
 
 #include "ctest.h"
 
-int main(int argc, const char *argv[])
-{
-    int result = ctest_main(argc, argv);
-
-    printf("\nNOTE: some tests will fail, just to show how ctest works! ;)\n");
-    return result;
+int main(int argc, const char *argv[]) {
+  int result = ctest_main(argc, argv);
+  return result;
 }
 
